b7610198533ada24cb72d478f55b1d5d5f6e4fc3
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-08 00:10:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,19 @@
+<<<<<<< HEAD
+=======
+numbers = [1,2,3]
+
+>>>>>>> 5c9c1789f2e93b36d09151758e985b84f88196e1
 def square_array(numbers)
   # your code here
   squares = []
   numbers.each do |number| 
+<<<<<<< HEAD
     puts squares.push(number**2)
   end
   return squares
+=======
+    number**2
+    squares.push
+  end
+>>>>>>> 5c9c1789f2e93b36d09151758e985b84f88196e1
 end
